48534eda3f9989ff7d3633b59359a1d7cb2ef603
AnikitaMaia <110617264+AnikitaMaia@users.noreply.github.com>
2024-06-05 11:30:06 -0300

    Add files via upload

---
+++ man.js
@@ -0,0 +1,13 @@
+let nomeUsuario = "";
+let elemento = document. querySelector(nome-usuario);
+
+
+while(nomeUsuario==""){
+    prompt("Qual o seu nome?");
+}
+
+if(nomeUsuario==null){
+    elemento.textContent = "seja muito bem vindo";
+}else{
+    elemento.textContent = nomeUsuario;
+}
